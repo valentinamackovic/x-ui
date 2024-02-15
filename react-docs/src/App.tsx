39164980f162f296fee7d4d7c554ea156c35f70d
Valentina Mackovic <valentina.mackovic@tymeshift.com>
2024-02-15 14:48:14 +0100

    Add react demo app

---
+++ react-docs/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './shared.css';
+
+function App() {
+  return (
+    <>
+      <title className="title">X UI</title>
+      <div className="divider"></div>
+      <div>
+        React docs
+      </div></>
+  );
+}
+
+export default App;
