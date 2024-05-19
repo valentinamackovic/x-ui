7cae82a2eb7e55bcb96c4ba55a33f31ebfc7a71a
Valentina Mackovic <mackovicvalentina@gmail.com>
2024-05-19 20:06:17 +0200

    Add support for static prop for menu component

--- react-docs/src/pages/MenuPage.tsx
+++ react-docs/src/pages/MenuPage.tsx
@@ -1,5 +1,29 @@
+import { useState } from "react";
 import Example from "../components/Example";
 import { Menu } from "../xui";
+
+const StaticMenuExample = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="center">
+      <Menu
+        isStatic
+        open={isOpen}
+        onClose={() => setIsOpen(false)}
+        items={[
+          { id: "1", value: "Aciton 1 action 1" },
+          { id: "2", value: "Aciton 2" },
+          { id: "3", value: "Aciton 3" },
+        ]}
+        onItemClick={(item) => window.alert("item was clciked: " + item)}
+        onButtonClick={() => setIsOpen(true)}
+      >
+        Options
+      </Menu>
+    </div>
+  );
+};
 
 const MenuPage = () => {
   return (
@@ -8,6 +32,7 @@ const MenuPage = () => {
       <p>A versatile menu for navigation.</p>
       <h2>Examples</h2>
       <h3>Use as a component</h3>
+      <p>Basic example</p>
       <Example
         exampleContent={
           <div className="center">
@@ -17,6 +42,7 @@ const MenuPage = () => {
                 { id: "2", value: "Aciton 2" },
                 { id: "3", value: "Aciton 3" },
               ]}
+              onItemClick={(item) => window.alert("item was clciked: " + item)}
             >
               Options
             </Menu>
@@ -24,6 +50,65 @@ const MenuPage = () => {
         }
         codeContent="a"
       />
+      <p>Disabled items</p>
+      <Example
+        exampleContent={
+          <div className="center">
+            <Menu
+              disabledItems={["1", "2"]}
+              items={[
+                { id: "1", value: "Aciton 1 action 1" },
+                { id: "2", value: "Aciton 2" },
+                { id: "3", value: "Aciton 3" },
+              ]}
+              onItemClick={(item) => window.alert("item was clciked: " + item)}
+            >
+              Options
+            </Menu>
+          </div>
+        }
+        codeContent="a"
+      />
+      <p>Open set to true</p>
+      <Example
+        exampleContent={
+          <div className="center">
+            <Menu
+              open
+              items={[
+                { id: "1", value: "Aciton 1 action 1" },
+                { id: "2", value: "Aciton 2" },
+                { id: "3", value: "Aciton 3" },
+              ]}
+              onItemClick={(item) => window.alert("item was clciked: " + item)}
+            >
+              Options
+            </Menu>
+          </div>
+        }
+        codeContent="a"
+      />
+      <p>Trigger a function when the menu popup is closed</p>
+      <Example
+        exampleContent={
+          <div className="center">
+            <Menu
+              onClose={() => window.alert("Closed menu popup.")}
+              items={[
+                { id: "1", value: "Aciton 1 action 1" },
+                { id: "2", value: "Aciton 2" },
+                { id: "3", value: "Aciton 3" },
+              ]}
+              onItemClick={(item) => window.alert("item was clciked: " + item)}
+            >
+              Options
+            </Menu>
+          </div>
+        }
+        codeContent="a"
+      />
+      <p>Ignore the internal logic with static property</p>
+      <Example exampleContent={<StaticMenuExample />} codeContent="a" />
     </>
   );
 };
--- react-docs/src/xui/Menu/Menu.tsx
+++ react-docs/src/xui/Menu/Menu.tsx
@@ -10,21 +10,40 @@ interface MenuProps {
   open?: boolean;
   onClose?: () => void;
   disabledItems?: string[] | number[];
-  selectedItem?: Item;
   items: Item[];
   onItemClick?: (id: string | number) => void;
+  onButtonClick?: () => void;
   children: React.ReactNode;
+  isStatic?: boolean;
+  // selectedItem???
 }
 
 export function Menu({
   disabledItems,
-  selectedItem,
   items,
   onItemClick,
+  onButtonClick,
   children,
+  open,
+  onClose,
+  isStatic,
 }: MenuProps) {
-  const [areOptionsVisible, setAreOptionsVisible] = useState(false);
+  const [areOptionsVisible, setAreOptionsVisible] = useState(open ?? false);
   const dropdownRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    open !== undefined && setAreOptionsVisible(open);
+  }, [open]);
+
+  const close = () => {
+    !isStatic && setAreOptionsVisible(false);
+    onClose?.();
+  };
+
+  const onMenuButtonClick = () => {
+    !isStatic && setAreOptionsVisible(!areOptionsVisible);
+    onButtonClick?.();
+  };
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
@@ -32,12 +51,12 @@ export function Menu({
         dropdownRef.current &&
         !dropdownRef.current.contains(event.target as Node)
       ) {
-        setAreOptionsVisible(false);
+        close();
       }
     };
 
     const handleKeyDown = (event: KeyboardEvent) => {
-      if (event.keyCode === 27) setAreOptionsVisible(false); // ESC key
+      if (event.keyCode === 27) close(); // ESC key
     };
 
     // Add when the component mounts
@@ -53,23 +72,30 @@ export function Menu({
 
   return (
     <div className="menu-wrapper">
-      <button
-        className="menu-button"
-        onClick={() => setAreOptionsVisible(!areOptionsVisible)}
-      >
+      <button className="menu-button" onClick={onMenuButtonClick}>
         {children}
       </button>
       {areOptionsVisible && (
         <div ref={dropdownRef} className="menu-dropdown">
-          {items.map((item) => (
-            <div
-              key={item.id}
-              className="menu-option"
-              onClick={() => onItemClick?.(item.id)}
-            >
-              {item.value}
-            </div>
-          ))}
+          {items.map((item) => {
+            const disabledIndex = disabledItems?.findIndex(
+              (disabledItem: string | number) => item.id === disabledItem
+            );
+            console.log({ item, disabledItems, disabledIndex });
+
+            const isDisabled =
+              disabledIndex !== undefined ? disabledIndex > -1 : false;
+
+            return (
+              <div
+                key={item.id}
+                className={`menu-option ${isDisabled ? "disabled" : ""}`}
+                onClick={() => !isDisabled && onItemClick?.(item.id)}
+              >
+                {item.value}
+              </div>
+            );
+          })}
         </div>
       )}
     </div>
