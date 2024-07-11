877ba7e8d9f6212232e2b1f5e83b2e00b068e662
Valentina Mackovic <mackovicvalentina@gmail.com>
2024-07-11 11:24:23 +0200

    Accordion (svelte) :done:

---
+++ svelte-docs/src/routes/accordion/+page.svelte
@@ -0,0 +1,77 @@
+<script lang="ts">
+import Example from "../../components/Example.svelte";
+import Accordion from "../../xui/accordion/Accordion.svelte";
+  import ComposableAccordion from "./ComposableAccordion.svelte";
+
+let expanded = false
+
+const onClick = () => {
+    expanded = !expanded
+}
+</script>
+
+<h1 class="main-page-content-title">Accordion</h1>
+<p>A content area which can be collapsed and expanded.</p>
+<h2>Examples</h2>
+<h3>Use as a component</h3>
+<p>Basic example</p>
+<Example codeContent="g">
+    <div slot="exampleContent">  
+        <Accordion 
+            title="Lorem ipsum"
+            component={true}
+        >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor quos unde
+            suscipit quisquam tenetur, autem dolore labore sapiente explicabo omnis
+            iusto amet repudiandae dignissimos dolores ex tempora fugit eum maiores.
+        </Accordion>
+    </div>
+</Example>
+<p>Initial state set to expanded</p>
+<Example codeContent="g">
+    <div slot="exampleContent">  
+        <Accordion 
+            title="Lorem ipsum"
+            expanded={true}
+            component={true}
+        >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor quos unde
+            suscipit quisquam tenetur, autem dolore labore sapiente explicabo omnis
+            iusto amet repudiandae dignissimos dolores ex tempora fugit eum maiores.
+        </Accordion>
+    </div>
+</Example>
+<p>Example without a title.</p>
+<Example codeContent="g">
+    <div slot="exampleContent">  
+        <Accordion 
+            component={true}
+        >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor quos unde
+            suscipit quisquam tenetur, autem dolore labore sapiente explicabo omnis
+            iusto amet repudiandae dignissimos dolores ex tempora fugit eum maiores.
+        </Accordion>
+    </div>
+</Example>
+<p>Ignore the internal logic with static property</p>
+<Example codeContent="g">
+    <div slot="exampleContent">  
+        <Accordion
+            expanded={expanded}
+            onTitleClick={onClick}
+            title="Lorem ipsum"
+            component={true}
+            isStatic={true}
+        >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor quos unde
+            suscipit quisquam tenetur, autem dolore labore sapiente explicabo omnis
+            iusto amet repudiandae dignissimos dolores ex tempora fugit eum maiores.
+        </Accordion>
+    </div>
+</Example>
+<h3>Use as a composable</h3>
+<Example codeContent="g">
+    <div slot="exampleContent">  
+        <ComposableAccordion />
+    </div>
+</Example>
---
+++ svelte-docs/src/routes/accordion/ComposableAccordion.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+import Accordion from '../../xui/accordion/Accordion.svelte';
+import AccordionButton from '../../xui/accordion/AccordionButton.svelte';
+import AccordionContent from '../../xui/accordion/AccordionContent.svelte';
+
+let expanded = false;
+
+function toggleExpanded() {
+    expanded = !expanded;
+}
+</script>
+
+<div class="center column">
+    <Accordion>
+        <AccordionButton onClick={toggleExpanded}>
+            Basic example
+        </AccordionButton>
+        <AccordionContent expanded={expanded}>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Exercitationem deleniti corporis rerum vel temporibus perspiciatis
+            enim earum. Aliquid consequatur ipsa voluptatibus, earum quibusdam
+            atque saepe inventore, cupiditate unde, reprehenderit repellat!
+        </AccordionContent>
+    </Accordion>
+</div>
+
+<style>
+.center {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.column {
+    flex-direction: column;
+}
+</style>
+  
