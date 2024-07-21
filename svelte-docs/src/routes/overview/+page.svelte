f9a1c6898d853a69735928e11919318cdc1bec56
Valentina Mackovic <mackovicvalentina@gmail.com>
2024-07-21 16:58:00 +0200

    Add overview page for svelte docs

---
+++ svelte-docs/src/routes/overview/+page.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+import { goto } from '$app/navigation';
+
+function goTo(route: string) {
+  goto(route);
+}
+</script>
+
+<main>
+    <h1 class="main-page-content-title">Overview</h1>
+    <p>
+      X UI offers ready-to-use components that are both easy to implement and
+      flexible, allowing for enhanced control and composability.
+    </p>
+    <div class="overview-wrapper">
+      <a class="overview-item" href="/menu">
+        <h3>Menu</h3>
+        <div>
+          <img alt="menu" src="/menuExample.png" class="overview-image"/>
+        </div>
+        </a>
+      <a class="overview-item" href="/select">
+        <h3>Select</h3>
+        <div>
+          <img alt="select" src="/selectExample.png" class="overview-image"/>
+        </div>
+        </a>
+      <a class="overview-item" href="/accordion">
+        <h3>Accordion</h3>
+        <div>
+          <img alt="accordion" src="/accordionExample.png" class="overview-image" />
+        </div>
+      </a>
+      <a class="overview-item" href="/modal">
+        <h3>Modal</h3>
+        <div>
+          <img alt="modal" src="/modalExample.png" class="overview-image" />
+        </div>
+      </a>
+      <a class="overview-item" href="/multiselect">
+        <h3>Multiselect</h3>
+        <div>
+          <img alt="multiselect" src="/multiselectExample.png" class="overview-image" />
+        </div>
+      </a>
+    </div>
+  </main>
+
+<style>
+a {
+    text-decoration: none;
+    color: inherit;
+}
+</style>
