13d90ab4ae864581a16581398ddc6fe8c12908cb
Valentina Mackovic <mackovicvalentina@gmail.com>
2024-07-22 13:54:05 +0200

    Fix routing issues in svelte #3

--- svelte-docs/svelte.config.js
+++ svelte-docs/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,11 +8,22 @@ const config = {
   preprocess: vitePreprocess(),
 
   kit: {
-    adapter: adapter(),
-    prerender: {
-      default: true,
-    },
+    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
+    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+    adapter: adapter({
+      // default options are shown. On some platforms
+      // these options are set automatically — see below
+      pages: "build",
+      assets: "build",
+      fallback: undefined,
+      precompress: false,
+      strict: true,
+      fallback: "200.html",
+    }),
   },
 };
+
+export const prerender = true;
 
 export default config;
