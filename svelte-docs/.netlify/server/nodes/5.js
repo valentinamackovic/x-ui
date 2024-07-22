f9521cdb331d8b69046dcdcdb9c1ce8700ea4c6b
Valentina Mackovic <mackovicvalentina@gmail.com>
2024-07-22 14:12:09 +0200

    Fix routing issues in svelte, netlify adapter

---
+++ svelte-docs/.netlify/server/nodes/5.js
@@ -0,0 +1,8 @@
+
+
+export const index = 5;
+let component_cache;
+export const component = async () => component_cache ??= (await import('../entries/pages/modal/_page.svelte.js')).default;
+export const imports = ["_app/immutable/nodes/5.C5x5wk0p.js","_app/immutable/chunks/scheduler.CYKcrEwl.js","_app/immutable/chunks/index.CrVH0I0K.js","_app/immutable/chunks/ComponentApiTable.D-CvUVM2.js","_app/immutable/chunks/index.Bc9b-_1R.js"];
+export const stylesheets = ["_app/immutable/assets/5.BhAniHfy.css","_app/immutable/assets/ComponentApiTable.CxCLBsHh.css"];
+export const fonts = [];
--- svelte-docs/svelte.config.js
+++ svelte-docs/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-netlify";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,19 +8,16 @@ const config = {
   preprocess: vitePreprocess(),
 
   kit: {
-    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
-    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+    // default options are shown
     adapter: adapter({
-      // default options are shown. On some platforms
-      // these options are set automatically — see below
-      pages: "build",
-      assets: "build",
-      fallback: undefined,
-      precompress: false,
-      strict: true,
-      fallback: "200.html",
-      trailingSlash: "always",
+      // if true, will create a Netlify Edge Function rather
+      // than using standard Node-based functions
+      edge: false,
+
+      // if true, will split your app into multiple functions
+      // instead of creating a single one for the entire app.
+      // if `edge` is true, this option cannot be used
+      split: false,
     }),
   },
 };
